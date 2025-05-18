9e520da84e47b834e96a238d2fef6458421e2cb6
Emilien Caudron <129062731+Ecaudron@users.noreply.github.com>
2025-05-18 22:39:34 +0200

    Update evaluation functions

--- evaluation.py
+++ evaluation.py
@@ -1,18 +1,34 @@
 
 from semantic_tagging import read_pdf, read_word, predict_tags
 import os
+import pandas as pd
 
 INPUT_PATH = ""
 
 def comparing_to_ground_truth(relevant_tags, ground_truth):
-    """TO DO"""
+    """"""
+    number_of_tags = len(ground_truth)
+    accuracy = 0
+
+    for tag in relevant_tags:
+        if tag in ground_truth:
+            accuracy += 1
+
+    return accuracy/number_of_tags
 
 def main():
 
     # Loop through the test corpus 
     format_error = []
-    for file in os.listdir(INPUT_PATH):
+
+    testing_corpus = pd.read_excel(INPUT_PATH)
+    accuracy_list = []
+    prediction_list = []
+    
+    #for file in os.listdir(INPUT_PATH):
+    for index, row in testing_corpus.iterrows():
         
+        file = row[""]
         # Extracting the text from the file
         if file.endswith(".docx"):
             text = read_word(file)
@@ -22,8 +38,17 @@ def main():
             print("Wrong format")
             format_error.append(file)
 
+        text = row[""]
+        ground_truth = row[""]
+
         # Predicting tags on the extracted text
         predicted_tags = predict_tags(text)
-        comparing_to_ground_truth()
+        accuracy = comparing_to_ground_truth(predict_tags, ground_truth)
+
+        prediction_list.append(predicted_tags)
+        accuracy_list.append(accuracy)
+    
+    testing_corpus["Predictions"] = prediction_list
+    testing_corpus["Accuracy"] = accuracy_list
 
 
