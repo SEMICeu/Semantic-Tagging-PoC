30ce9cedb2873c5809241bc95ab4cc38bedb5938
Emilien Caudron <129062731+Ecaudron@users.noreply.github.com>
2025-05-21 22:43:24 +0200

    update evaluation script

--- evaluation.py
+++ evaluation.py
@@ -2,6 +2,9 @@
 from semantic_tagging import read_pdf, read_word, predict_tags
 import os
 import pandas as pd
+import PyPDF2
+import requests
+import io
 
 INPUT_PATH = ""
 
@@ -14,7 +17,7 @@ def comparing_to_ground_truth(relevant_tags, ground_truth):
         if tag in ground_truth:
             accuracy += 1
 
-    return accuracy/number_of_tags
+    return accuracy/number_of_tags, accuracy / len(relevant_tags)
 
 def main():
 
@@ -22,33 +25,44 @@ def main():
     format_error = []
 
     testing_corpus = pd.read_excel(INPUT_PATH)
-    accuracy_list = []
+    recall_list = []
+    precision_list = []
     prediction_list = []
     
     #for file in os.listdir(INPUT_PATH):
     for index, row in testing_corpus.iterrows():
         
-        file = row[""]
+        file = row["URL"]
         # Extracting the text from the file
-        if file.endswith(".docx"):
-            text = read_word(file)
-        elif file.endswith(".pdf"):
-            text = read_pdf(file)
+        response = requests.get(file)
+        if response.status_code == 200:
+            pdf_file = io.BytesIO(response.content)
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            text = ""
+            for page in pdf_reader.pages: 
+                text += page.extract_text() + "\n"
         else:
             print("Wrong format")
             format_error.append(file)
 
-        text = row[""]
-        ground_truth = row[""]
+        text = row["Article summary "] # row["Article Title"]
+        ground_truth = row["DET tags that express the main subject matter (often with post-coordination)"].split(" + ")
 
         # Predicting tags on the extracted text
         predicted_tags = predict_tags(text)
-        accuracy = comparing_to_ground_truth(predict_tags, ground_truth)
+        recall, precision = comparing_to_ground_truth(predict_tags, ground_truth)
 
         prediction_list.append(predicted_tags)
-        accuracy_list.append(accuracy)
+        recall_list.append(recall)
+        precision_list.append(precision)
     
-    testing_corpus["Predictions"] = prediction_list
-    testing_corpus["Accuracy"] = accuracy_list
+    testing_corpus["Label created by Auto-tagger"] = prediction_list
+    testing_corpus["Recall"] = recall_list
+    testing_corpus["Precision"] = precision_list
+
+    testing_corpus.to_excel("results/results.xlsx")
+
+if __name__=="__main__":
+    main()
 
 
